74781bb2a66876f49526042d997f1364c9d38fcd
Bryan Foster <bpfoster85@gmail.com>
2022-03-14 13:06:40 -0600

    big bang

---
+++ simpleloadtesting/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace simpleloadtesting.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
